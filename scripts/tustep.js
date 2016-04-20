c092a7ad90c397f676cf6b682ba18c39b88f7359
Alejandro Benito <abenito@usal.es>
2016-04-20 11:01:42 +0200

    Updated tustep script to extract places to a file

--- scripts/tustep.js
+++ scripts/tustep.js
@@ -19,6 +19,75 @@ if (process.argv[2] == 'undefined' || process.argv[2].length == 0) {
     console.warn('You must provide a root directory of XML files')
 }
 
+function insertOrt(place) {
+    if (places.hasOwnProperty(place)) {
+        var count = places[place];
+        places[place] = count + 1;
+    } else {
+        places[place] = 1;
+    }
+}
+function processHLField(theField, file) {
+    var lemma = theField["_"];
+    if (lemma) {
+        lemma = lemma.replace(" ", "");
+        lemma = lemma.toLowerCase();
+        // console.log('Lemma found: ' + lemma);
+        if (lemmas.hasOwnProperty(lemma)) {
+            if (lemmas[lemma].hasOwnProperty(file)) {
+                var num = lemmas[lemma][file];
+                lemmas[lemma][file] = num + 1;
+            } else {
+                lemmas[lemma][file] = 1;
+            }
+            lemmas[lemma]["count"] += 1;
+
+
+        } else {
+            lemmas[lemma] = {};
+            lemmas[lemma]["count"] = 1;
+            lemmas[lemma][file] = 1;
+        }
+    }
+
+    return lemma;
+}
+
+function processQDBField(theField, lemma) {
+
+    if (theField.hasOwnProperty("field")) {
+        var ortField = theField.field[0];
+        var ortName;
+        if (ortField["$"].name == "O") {
+            placesCount++;
+            ortName = ortField["_"];
+            insertOrt(ortName);
+        }
+    }
+    var match = theField["_"].match(/\d{4}/);
+    if (match == 'undefined' || match == null) {
+        if (lemmas[lemma].hasOwnProperty("year")) {
+            if (lemmas[lemma]["year"].indexOf('unknown') == -1)
+                lemmas[lemma]["year"].push('unknown');
+        } else {
+            lemmas[lemma]["year"] = [];
+            lemmas[lemma]["year"].push('unknown');
+        }
+    } else {
+        match.forEach(function (theMatch, idx) {
+            if (idx == 0) {
+                if (lemmas[lemma].hasOwnProperty("year")) {
+                    if (lemmas[lemma]["year"].indexOf(theMatch) == -1)
+                        lemmas[lemma]["year"].push(theMatch);
+                } else {
+                    lemmas[lemma]["year"] = [];
+                    lemmas[lemma]["year"].push(theMatch);
+                }
+
+            }
+        });
+    }
+}
 if(fs.statSync(process.argv[2]).isFile()) {
     finalObj = require(process.argv[2]);
 
@@ -212,8 +281,13 @@ if(fs.statSync(process.argv[2]).isFile()) {
     console.log('Found ' + JSON.stringify(subdirs));
 
     var lemmas = {};
-    var counter = 0;
+    var places = {};
+    var placesCount = 0;
+    var showNumber = false;
     subdirs.forEach(function(subdir) {
+        if (subdir.indexOf('n-o-p') >  -1) {
+            showNumber = true;
+        } else showNumber = false;
         var letterPath = path.join(dir, subdir);
         var files = glob.sync(letterPath + '/*2-3.xml');
         console.log(files.length + ' XML files found in directory ' + letterPath);
@@ -224,63 +298,30 @@ if(fs.statSync(process.argv[2]).isFile()) {
                 // var jsonResult = JSON.stringify(result);
                 if (result.hasOwnProperty("records")) {
                     if (result.records.hasOwnProperty("record")) {
+                        console.log(file);
                         result.records.record.forEach(function(record) {
                             if (record.hasOwnProperty("field")) {
                                 var lemma;
-                                record["field"].forEach(function(theField) {
-                                    if(theField["$"].name == "HL") {
-                                        lemma = theField["_"];
-                                        if (lemma) {
-                                            lemma = lemma.replace(" ", "");
-                                            lemma = lemma.toLowerCase();
-                                            // console.log('Lemma found: ' + lemma);
-                                            if (lemmas.hasOwnProperty(lemma)) {
-                                                if (lemmas[lemma].hasOwnProperty(file)) {
-                                                    var num = lemmas[lemma][file];
-                                                    lemmas[lemma][file] = num + 1;
-                                                } else {
-                                                    lemmas[lemma][file] = 1;
-                                                }
-                                                lemmas[lemma]["count"] += 1;
-
-
-                                            } else {
-                                                lemmas[lemma] = {};
-                                                lemmas[lemma]["count"] = 1;
-                                                lemmas[lemma][file] = 1;
-                                            }
-                                        }
-                                    } else if (theField["$"].name == "QDB") {
-                                        if (lemma == null || lemmas[lemma] == null || lemmas[lemma] == 'undefined') {
-                                            console.log('Lemma is null');
-
-                                        } else {
-                                            var match = theField["_"].match(/\d{4}/);
-                                            if (match == 'undefined' || match == null) {
-                                                if (lemmas[lemma].hasOwnProperty("year")) {
-                                                    if (lemmas[lemma]["year"].indexOf('unknown') == -1)
-                                                        lemmas[lemma]["year"].push('unknown');
-                                                } else {
-                                                    lemmas[lemma]["year"] = [];
-                                                    lemmas[lemma]["year"].push('unknown');
-                                                }
-                                            } else {
-                                                match.forEach(function(theMatch, idx) {
-                                                    if (idx == 0) {
-                                                        if (lemmas[lemma].hasOwnProperty("year")) {
-                                                            if (lemmas[lemma]["year"].indexOf(theMatch) == -1)
-                                                                lemmas[lemma]["year"].push(theMatch);
-                                                        } else {
-                                                            lemmas[lemma]["year"] = [];
-                                                            lemmas[lemma]["year"].push(theMatch);
-                                                        }
-
-                                                    }
-                                                });
-                                            }
-                                        }
-                                    }
+                                var theField = _.find(record["field"], function(aField) {
+                                    return aField["$"].name == "HL";
                                 });
+                                if (theField)
+                                    lemma = processHLField(theField, file);
+
+                                theField = _.find(record["field"], function(aField) {
+                                    return aField["$"].name == "QDB";
+                                });
+                                if (theField && lemma && lemma !== 'undefined' && lemma.length > 1)
+                                    processQDBField(theField, lemma);
+
+                                theField = _.find(record["field"], function(aField) {
+                                    return aField["$"].name == "O";
+                                });
+                                if (theField) {
+                                    placesCount++;
+                                    var place = theField["_"];
+                                    insertOrt(place);
+                                }
                             } else {
                                 console.log('Record has no fields');
                             }
@@ -291,11 +332,24 @@ if(fs.statSync(process.argv[2]).isFile()) {
         });
     });
 
-    fs.writeFile('results.json', JSON.stringify(lemmas),  function(err) {
-        if (err) throw err;
-        console.log('It\'s saved!');
-        process.exit(0);
+    var placesArray = [];
+
+    _.each(places, function(value, key) {
+        placesArray.push({"name": key, "count": value});
     });
+
+    placesArray = _.sortBy(placesArray, "count");
+
+    fs.writeFile('places.json', JSON.stringify(placesArray, null, 2), function(err) {
+            if (err) throw err;
+            console.log('It\'s saved!');
+            process.exit(0);
+    });
+    // fs.writeFile('results.json', JSON.stringify(lemmas),  function(err) {
+    //     if (err) throw err;
+    //     console.log('It\'s saved!');
+    //     process.exit(0);
+    // });
 }
 
 
