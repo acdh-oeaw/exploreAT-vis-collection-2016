0d48bb861d925c45cd2d495918c27aad67e24eb3
Alejandro Benito <abenito@usal.es>
2016-05-18 12:21:29 +0200

    Code clean-up

--- public/javascripts/tustep_map.js
+++ public/javascripts/tustep_map.js
@@ -9,21 +9,6 @@ var cartoMap;
     var geohashBuckets = [];
     var zoom = 8;
 
-    var geohashFeatures;
-
-
-
-    // var map = L.map('map').setView([47.333333, 13.333333], 7);
-    // var mapLink =
-    //     '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-    // L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-    //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://exploreat.usal.es">ExploreAT!</a>',
-    //     subdomains: 'abcd',
-    //     maxZoom: 14,
-    //     minZoom: 8,
-    //     detectRetina: true
-    // }).addTo(map);
-
     var svg,
         featureLayer;
 
@@ -60,58 +45,37 @@ var cartoMap;
     cartoMap.addCartoLayer(terrainLayer);
     cartoMap.addCartoLayer(geojsonLayer);
 
+    cartoMap.setScale(6);
+
+    cartoMap.centerOn([13.333333, 47.333333],"latlong");
+
+    update();
 
 
-    d3.json("data/austria.json", function (json) {
-        function style(feature) {
-            return {
-                fillColor: "#E3E3E3",
-                weight: 1,
-                opacity: 1,
-                color: 'black',
-                fillOpacity: 0.5,
-                "pointer-events": "none"
-            };
-        }
+    function update() {
+        getDataForZoomLevel(zoom)
+            .then(function(resp) {
+                var features = generateFeatures(resp);
 
-        // L.geoJson(json, {
-        //     // onEachFeature: onEachFeature,
-        //     style: style
-        // }).addTo(map);
+                if (featureLayer == undefined) {
+                    featureLayer = d3.carto.layer.featureArray().label("SVG Features")
+                        .cssClass("featureLayer")
+                        .features(features)
+                        .renderMode("svg");
+                    cartoMap.addCartoLayer(featureLayer);
+                } else {
+                    featureLayer.features(features);
+                    cartoMap.refreshCartoLayer(featureLayer);
+                }
 
-        // function onEachFeature(feature, layer){
-        //     layer.on({
-        //         click : onCountryClick,
-        //         mouseover : onCountryHighLight,
-        //         mouseout : onCountryMouseOut
-        //     });
-        // }
-        // L.control.scale().addTo(map);
-
-        /* Initialize the SVG layer */
-        // map._initPathRoot();
-
-        /* We simply pick up the SVG from the map object */
-        // svg = d3.select("#map").select("svg");
-        // g = svg.append("g");
-
-        // map.on("viewreset", update);
-        // map.on("zoomstart", function () {
-        //     g.style("opacity", .0);
-        //     console.log('Zoom start');
-        //
-        // });
-        // map.on("click", function () {
-        //     console.log("mouse click");
-        // });
-        cartoMap.setScale(6);
-        cartoMap.centerOn([13.333333, 47.333333],"latlong");
-
-        // featureLayer = d3.carto.layer.featureArray().label("SVG Features")
-
-        update();
-    });
-
+                var minDocCount = _.min(features, function(el) {
+                    return el.properties.doc_count;
+                }).properties.doc_count;
+                var maxDocCount = _.max(features, function(el) {
+                    return el.properties.doc_count;
+                }).properties.doc_count;
+            });
+    }
 
 
     function generateFeatures(resp) {
@@ -149,98 +113,6 @@ var cartoMap;
         });
     }
 
-
-
-    function update() {
-        getDataForZoomLevel(zoom)
-            .then(function(resp) {
-                var features = generateFeatures(resp);
-                // var d3_features = g.selectAll(".geohashpath").data(features);
-
-                // if (featureLayer !== undefined)
-                //     cartoMap.deleteCartoLayer(featureLayer);
-
-                // featureLayer = d3.carto.layer.featureArray().label("SVG Features")
-                //     .cssClass("featureLayer")
-                //     .features(features)
-                //     .renderMode("svg");
-                //
-                // cartoMap.addCartoLayer(featureLayer);
-
-                if (featureLayer == undefined) {
-                    featureLayer = d3.carto.layer.featureArray().label("SVG Features")
-                        .cssClass("featureLayer")
-                        .features(features)
-                        .renderMode("svg");
-                    cartoMap.addCartoLayer(featureLayer);
-                } else {
-                    featureLayer.features(features);
-                    cartoMap.refreshCartoLayer(featureLayer);
-                }
-                
-                var minDocCount = _.min(features, function(el) {
-                    return el.properties.doc_count;
-                }).properties.doc_count;
-                var maxDocCount = _.max(features, function(el) {
-                    return el.properties.doc_count;
-                }).properties.doc_count;
-
-                // var sizeInPixels = map.getSize(map.getZoom());
-                //
-                // var radiusScale = d3.scale.linear()
-                //     .domain([minDocCount, maxDocCount])
-                //     .range([sizeInPixels.x * 0.01, sizeInPixels.x * 0.03]);
-
-                // var transform = d3.geo.transform({point: projectPoint}),
-                //     path = d3.geo.path().projection(transform);
-
-
-                // d3_features.attr("transform",
-                //     function(d) {
-                //         return "translate("+
-                //             map.latLngToLayerPoint(d.geometry.coordinates).x +","+
-                //             map.latLngToLayerPoint(d.geometry.coordinates).y +")";
-                //     })
-                //     .attr("r", function(d){return radiusScale(d.properties.doc_count);});
-
-                // d3_features
-                //     .attr("d", path);
-                //
-                // d3_features.enter()
-                //     .append("path")
-                //     .attr("class","geohashpath")
-                //     .attr("d", path)
-                //     .style("fill-opacity", 0.7)
-                //     .attr('fill','blue');
-
-                // d3_features
-                //     .enter()
-                //     .append("a")
-                //     .attr("transform",
-                //         function(d) {
-                //             return "translate("+
-                //                 map.latLngToLayerPoint(d.geometry.coordinates).x +","+
-                //                 map.latLngToLayerPoint(d.geometry.coordinates).y +")";
-                //         }
-                //     ).style("stroke", "black")
-                //     .style("opacity", .6)
-                //     .style("fill", "red")
-                //     .attr("r", function(d){return radiusScale(d.properties.doc_count);})
-                //     .on('mouseover', function() {console.log('mouseover')});
-
-
-
-
-                // g.style("opacity", 1);
-
-                // d3_features.exit().remove();
-            });
-    }
-
-    // function projectPoint(x, y) {
-    //     var point = map.latLngToLayerPoint(new L.LatLng(x, y));
-    //     this.stream.point(point.x, point.y);
-    // }
 
     function getDataForZoomLevel(zoomLevel) {
 
