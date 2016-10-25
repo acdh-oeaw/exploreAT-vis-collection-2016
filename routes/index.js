ebe9ed48f3f04c6465a31db94fd8a44dab39bf04
Alejandro Benito <abenito@usal.es>
2016-10-25 11:27:41 +0200

    Fixed routes

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.get('/profile', isLoggedIn, function(req, res) {
 
 router.get('/logout', function(req, res) {
     req.logout();
-    res.redirect('/');
+    res.redirect('./');
 });
 
 // router.get('/ex_persons', function(req,res) {
@@ -83,13 +83,13 @@ router.get('/ex_tustep_map', isLoggedIn, function(req,res) {
 
 router.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/',
-    failureRedirect: '/signup',
+    failureRedirect: './signup',
     failureFlash: true
 }));
 
 router.post('/login', passport.authenticate('local-login', {
     successRedirect: '/',
-    failureRedirect: '/login',
+    failureRedirect: './login',
     failureFlash: true
 }));
 
@@ -99,5 +99,5 @@ module.exports = router;
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
-    res.redirect('/login');
+    res.redirect('./login');
 }
