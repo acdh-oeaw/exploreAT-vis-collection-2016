0a503e37ed1cb4dfd63102c9dce74dacf75a7b7b
Alejandro Benito <abenito@usal.es>
2016-10-25 13:14:00 +0200

    Fixed views relative urls

--- routes/index.js
+++ routes/index.js
@@ -81,14 +81,14 @@ router.get('/ex_tustep_map', isLoggedIn, function(req,res) {
 //     res.render('ex_colors');
 // });
 
-router.post('signup', passport.authenticate('local-signup', {
-    successRedirect: '/',
+router.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: './',
     failureRedirect: 'signup',
     failureFlash: true
 }));
 
-router.post('login', passport.authenticate('local-login', {
-    successRedirect: '/',
+router.post('/login', passport.authenticate('local-login', {
+    successRedirect: './',
     failureRedirect: 'login',
     failureFlash: true
 }));
