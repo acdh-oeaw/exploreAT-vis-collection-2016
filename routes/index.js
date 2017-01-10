1991610cbf70f18e1721b8311d0b11f3bfb11652
Alejandro Benito <abenito@usal.es>
2017-01-10 14:09:49 +0100

    Fixed error in main page - Now using authentication

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/', function(req,res) {
+router.get('/', isLoggedIn, function(req,res) {
     res.render('index', { user : req.user });
 });
 
