729965eb3610eb6a5c614ff13f6adf4671a2190e
Alejandro Benito <abenito@usal.es>
2017-03-02 13:52:22 +0100

    Added autocompletion to questionnaire input field - Better queries - Reset Vis

--- public/javascripts/questionnaire_explorer.js
+++ public/javascripts/questionnaire_explorer.js
@@ -12,8 +12,6 @@ var mainExports = {};
 
     var indexName = 'tei-index';
 
-
-
     var svg = d3.select("#chordSVG"),
         width = +svg.attr("width"),
         height = +svg.attr("height"),
@@ -34,10 +32,124 @@ var mainExports = {};
 
     var color = d3.scaleOrdinal(d3.schemeCategory20);
 
+    $('#questionnaireSelector').autocomplete({
+        source: function (request, response) {
+
+            esClient.search({
+                index: indexName,
+                type: 'tei-type',
+                headers: {
+                    'Authorization' : "Bearer " + ESToken},
+                size: 10000,
+                body: {
+                    "query": {
+                        "match": {
+                            "questionnaire": {
+                                "query": request.term
+                            }
+                        }
+                    },
+                    "aggs": {
+                        "questionnaires": {
+                            "terms": {
+                                "field": "questionnaire.keyword",
+                                "min_doc_count": 50,
+                                "order": {
+                                    "_count": "desc"
+                                }
+                            }
+                        }
+                    }
+                }
+            }).then(function (resp) {
+                console.log(resp);
+                response(_.pluck(resp.aggregations.questionnaires.buckets, "key"));
+               // response(_.uniq(resp.hits.aggs.senses, false, function (d) {
+               //     return d._source.questionnaire;
+               // }).slice(0, 9).map(function (d) {
+               //     return d._source.questionnaire
+               // }));
+            })
+        },
+        minLength: 4
+    });
+
+    $("#questionnaireSelector").on( "autocompleteselect", function( event, ui ) {
+        resetVis();
+        esClient.search({
+            index: indexName,
+            type: 'tei-type',
+            headers: {
+                'Authorization' : "Bearer " + ESToken},
+            size: 10000,
+            body: {
+                "size": 0,
+                "query": {
+                    "bool": {
+                        "must": [
+                            {
+                                "query_string": {
+                                    "analyze_wildcard": true,
+                                    "query": "*"
+                                }
+                            },
+                            {
+                                "exists": {
+                                    "field": "sense.keyword"
+                                }
+                            },
+                            {
+                                "exists": {
+                                    "field": "fullLemma.keyword"
+                                }
+                            },
+                            {
+                                "match": {
+                                    "questionnaire.keyword": {
+                                        "query": ui.item.value,
+                                        "type": "phrase"
+                                    }
+                                }
+                            }
+                        ],
+                        "must_not": []
+                    }
+                },
+                "_source": {
+                    "excludes": []
+                },
+                "aggs": {
+                    "senses": {
+                        "terms": {
+                            "field": "sense.keyword",
+                            "size": 50,
+                            "min_doc_count": 5,
+                            "order": {
+                                "_count": "desc"
+                            }
+                        },
+                        "aggs": {
+                            "lemmas": {
+                                "terms": {
+                                    "field": "fullLemma.keyword",
+                                    "size": 1000,
+                                    "order": {
+                                        "_count": "desc"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }).then(respFunction);
+    });
+
+
+
     //Word Cloud
 
     // Encapsulate the word cloud functionality
-// Encapsulate the word cloud functionality
     function wordCloud(selector) {
 
         var fill = color;
@@ -53,7 +165,7 @@ var mainExports = {};
         //Draw the word cloud
         function draw(words) {
             var cloud = svg.selectAll("g text")
-                .data(words, function(d) { return d.text; })
+                .data(words, function(d) { return d.text; });
 
             //Entering words
             cloud.enter()
@@ -70,7 +182,7 @@ var mainExports = {};
                 .attr("transform", function(d) {
                     return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
                 })
-                .style("fill-opacity", 1);;
+                .style("fill-opacity", 1);
 
             //Entering and existing words
             // cloud
@@ -95,7 +207,7 @@ var mainExports = {};
             //The outside world will need to call this function, so make it part
             // of the wordCloud return value.
             update: function(words) {
-                d3.cloud().size([500, 500])
+                d3.cloud().size([400, 400])
                     .words(words)
                     .padding(5)
                     .rotate(function() { return ~~(Math.random() * 2) * 90; })
@@ -108,141 +220,18 @@ var mainExports = {};
 
     }
 
-//Some sample data - http://en.wikiquote.org/wiki/Opening_lines
-    var words = [
-        "You don't know about me without you have read a book called The Adventures of Tom Sawyer but that ain't no matter.",
-        "The boy with fair hair lowered himself down the last few feet of rock and began to pick his way toward the lagoon.",
-        "When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.",
-        "It was inevitable: the scent of bitter almonds always reminded him of the fate of unrequited love."
-    ]
+    var resetVis = function () {
 
-//Prepare one of the sample sentences by removing punctuation,
-// creating an array of words and computing a random size attribute.
-    function getWords(i) {
-        return words[i]
-            .replace(/[!\.,:;\?]/g, '')
-            .split(' ')
-            .map(function(d) {
-                return {text: d, size: 10 + Math.random() * 60};
-            })
-    }
+        svg.selectAll('*').remove();
+
+        if (!!myWordCloud)
+            myWordCloud.update([]);
+    };
 
 
-//Create a new instance of the word cloud visualisation.
-    var myWordCloud = wordCloud('#chart');
-
-
-
-
-
-    // var fill = d3.scale.category20();
-
-    // var layout = d3.cloud()
-    //     .size([500, 500])
-    //     .words([
-    //         "Hello", "world", "normally", "you", "want", "more", "words",
-    //         "than", "this"].map(function(d) {
-    //         return {text: d, size: 10 + Math.random() * 90, test: "haha"};
-    //     }))
-    //     .padding(5)
-    //     .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    //     .font("Impact")
-    //     .fontSize(function(d) { return d.size; })
-    //     .on("end", draw);
-    //
-    // layout.start();
-
-    // function draw(words) {
-    //     d3.select("#cloudSVG")
-    //         .attr("width", layout.size()[0])
-    //         .attr("height", layout.size()[1])
-    //         .append("g")
-    //         .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-    //         .selectAll("text")
-    //         .data(words)
-    //         .enter().append("text")
-    //         .style("font-size", function(d) { return d.size + "px"; })
-    //         .style("font-family", "Impact")
-    //         .style("fill", function(d, i) { return color(i); })
-    //         .attr("text-anchor", "middle")
-    //         .attr("transform", function(d) {
-    //             return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-    //         })
-    //         .text(function(d) { return d.text; });
-    // }
-
-
-
-    esClient.search({
-        index: indexName,
-        type: 'tei-type',
-        headers: {
-            'Authorization' : "Bearer " + ESToken},
-        size: 10000,
-        body: {
-            "size": 0,
-            "query": {
-                "bool": {
-                    "must": [
-                        {
-                            "query_string": {
-                                "analyze_wildcard": true,
-                                "query": "*"
-                            }
-                        },
-                        {
-                            "exists": {
-                                "field": "sense.keyword"
-                            }
-                        },
-                        {
-                            "exists": {
-                                "field": "fullLemma.keyword"
-                            }
-                        },
-                        {
-                            "match": {
-                                "questionnaire.keyword": {
-                                    "query": "1A20: Kopf/Schädel als Gw. (Weiberkopf)",
-                                    "type": "phrase"
-                                }
-                            }
-                        }
-                    ],
-                    "must_not": []
-                }
-            },
-            "_source": {
-                "excludes": []
-            },
-            "aggs": {
-                "senses": {
-                    "terms": {
-                        "field": "sense.keyword",
-                        "size": 50,
-                        "min_doc_count": 5,
-                        "order": {
-                            "_count": "desc"
-                        }
-                    },
-                    "aggs": {
-                        "lemmas": {
-                            "terms": {
-                                "field": "fullLemma.keyword",
-                                "size": 1000,
-                                "order": {
-                                    "_count": "desc"
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }).then(function (resp) {
-
-        console.log(resp);
+    var respFunction = function (resp) {
         var sensesAgg = resp.aggregations['senses'].buckets;
+        console.log(resp);
 
         function zeros(dimensions) {
             var array = [];
@@ -257,37 +246,9 @@ var mainExports = {};
         var aMatrix = zeros([sensesAgg.length, sensesAgg.length]);
 
 
-        // var dict = {},
-        //     lemmas = [],
-        //     senses = [];
-
-        //Add possibility to sort senses here
-
-        // sensesAgg = _.sortBy(sensesAgg, "key");
-
         console.log(sensesAgg);
 
-        // _.forEach(sensesAgg, function (senseAgg, senseIdx) {
-        //     dict[senseAgg.key] = [];
-        //     senses.push(senseAgg.key);
-        //     _.forEach(senseAgg['lemmas'].buckets, function (lemmaAgg) {
-        //         var lemmaIdx = _.indexOf(lemmas, lemmaAgg.key);
-        //         if (lemmaIdx == -1) {
-        //             lemmas.push(lemmaAgg.key);
-        //             dict[senseAgg.key].push(lemmaAgg.key);
-        //             aMatrix[senseIdx][senseIdx] += lemmaAgg.doc_count;
-        //         } else {
-        //             _.forEach(_.allKeys(dict), function (senseKey) {
-        //                 if(_.contains(dict[senseKey], lemmaAgg.key)) {
-        //                     aMatrix[senseIdx][_.indexOf(senses, senseKey)] += lemmaAgg.doc_count;
-        //                 }
-        //             });
-        //         }
-        //     });
-        // });
 
-
-        // var uniqueMeaningLemmas = [];
         var senses = [];
 
         var sensesDict = {};
@@ -317,21 +278,7 @@ var mainExports = {};
             }
         }
 
-        // _.forEach(sensesAgg, function (senseAgg, senseIdx) {
-        //     var lemmas = senseAgg.lemmas;
-        //     _.forEach(lemmas, function (lemma) {
-        //
-        //     })
-        // });
-
-
         console.log(aMatrix);
-
-
-        // var color = d3.scaleOrdinal()
-        //     .domain(senses.length)
-        //     .range(["#000000", "#FFDD89", "#957244", "#F26223"]);
-
 
 
         var g = svg.append("g")
@@ -394,7 +341,13 @@ var mainExports = {};
             .style("stroke", function(d) { return d3.rgb(color(d.target.index)).darker(); });
 
 
-    });
+    };
+
+//Create a new instance of the word cloud visualisation.
+    var myWordCloud = wordCloud('#chart');
+
+
+
 
 
     function fade(opacity) {
